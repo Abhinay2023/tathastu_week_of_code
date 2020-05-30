0fea23b6a0e589e39ce9477e8696b5ef1bd2771b
Abhinay2023 <56387491+Abhinay2023@users.noreply.github.com>
2020-05-30 20:11:05 +0530

    Create Program4.py

---
+++ Day1/Program4.py
@@ -0,0 +1,6 @@
+cp=int(input("Enter cost price:"))
+sp=int(input("\nEnter selling price:"))
+profit=sp-cp
+print("\nProfit on sales is",profit)
+nsp=1.05*profit+cp
+print("Selling price after increasing profit by 5% is",nsp)
